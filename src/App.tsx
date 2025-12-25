2c4e4621f4531201f875a6f3a5a01cf5888b4d72
mohamedessayed <mlovely9316@gmail.com>
2025-12-25 22:29:52 +0200

    dynamic import

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,26 @@
 import { Route, Routes } from "react-router"
-import Movie from "./pages/Movie"
-import ViewMovie from "./pages/ViewMovie"
 import { FavoritesContextProvider } from "./context/FavoritesContext"
+import React, { Suspense } from "react"
+import LoadingLayout from "./components/Layout/LoadingLayout"
+
+const ViewMovie = React.lazy(() => import('./pages/ViewMovie'))
+const Movie = React.lazy(() => import('./pages/Movie'))
 
 function App() {
   return (
     <div>
 
       <Routes>
-        <Route index element={<Movie />} />
-        <Route path="/view/movie/:id" element={<FavoritesContextProvider><ViewMovie /></FavoritesContextProvider>} />
+        <Route index element={<Suspense fallback={<LoadingLayout />}><Movie /></Suspense>} />
+        <Route path="/view/movie/:id" element={<FavoritesContextProvider>
+
+          <Suspense fallback={<LoadingLayout />}>
+            <ViewMovie />
+          </Suspense>
+
+        </FavoritesContextProvider>} />
       </Routes>
-      
+
     </div>
   )
 }
--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,20 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [tailwindcss(), react()],
+
+  build:{
+    target:'esnext',
+    minify:'esbuild',
+    cssMinify:true,
+    emptyOutDir:true,
+    sourcemap:false,
+    chunkSizeWarningLimit:500,
+    rollupOptions:{
+      output:{
+        manualChunks:{
+          react:['react','react-dom'],
+        }
+      }
+    }
+  }
 })
